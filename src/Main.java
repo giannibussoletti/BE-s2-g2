62402d4ba4bae889a4c0276baa94ae1c81e040f1
giannibussoletti <bussolettigianni@gmail.com>
2026-06-09 09:38:44 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    static void main() {
+        //---------------COLLECTIONS------------
+        // Le collection sono dei tipi per organizzare dati e hanno sotto di loro 3 principali categorie
+        // LIST
+        // gli elementi nella list sono un elenco di cose, possiamo avere duplicati e possiamo aggiungere infiniti elementi
+        // SET
+        // Non ammettono duplicati a differenza delle LIST
+        // QUEUE (code)
+        // Rispetta una coda, come per esempio la coda di file di stampa, rispetta la gerarchia
+
+        //----------MAP----------
+        // Le map memorizzano i loro valori non singolarmente ma a coppie
+
+        //Le interfacce (List, Set, Map, Queue) ci dicono cosa possiamo fare, ma per
+        //creare un oggetto abbiamo bisogno delle classi concrete
+        //Ogni classe implementa la stessa interfaccia in modo diverso,
+        //ottimizzando prestazioni o comportamento
+    }
+}
