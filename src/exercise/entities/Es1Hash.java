6f223b943094db010b2575aed198f13debda95bd
giannibussoletti <bussolettigianni@gmail.com>
2026-06-09 17:09:53 +0200

    hash exercise little fix

--- src/exercise/entities/Es1Hash.java
+++ src/exercise/entities/Es1Hash.java
@@ -11,13 +11,16 @@ public class Es1Hash {
 
         System.out.println("Dimmi quante parole vuoi inserire");
         int nOfWords = Integer.parseInt(scanner.nextLine());
-
+        int totalDupli = 1;
         for (int i = 0; i < nOfWords; i++) {
             System.out.println("Dammi la " + (i + 1) + "°" + " parola");
             String word = scanner.nextLine();
-            if (randomWord.contains((word))) {
+            if (duplicateWord.contains(word)) {
+                totalDupli += 1;
+            } else if (randomWord.contains((word))) {
                 duplicateWord.add(word);
                 randomWord.remove(word);
+                totalDupli += 1;
             } else {
                 randomWord.add(word);
             }
@@ -25,8 +28,8 @@ public class Es1Hash {
         }
 
         System.out.println("Hai inserito " +
-                duplicateWord.size() +
-                " parole duplicate che sono:\n"
+                totalDupli +
+                " volte parole duplicate che sono:\n"
                 + duplicateWord + "\n"
                 + "Invece hai inserito "
                 + randomWord.size()
